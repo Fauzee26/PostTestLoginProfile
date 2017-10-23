bf18b852be01c5b4a6348b529a8043e77cbf9d53
Fauzee26 <32213481+Fauzee26@users.noreply.github.com>
2017-10-23 15:55:13 +0700

    Post Test Login Profile

---
+++ PostTestLoginProfile/PostTestLoginProfile/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  PostTestLoginProfile
+//
+//  Created by Muhammad Hilmy Fauzi on 23/10/17.
+//  Copyright © 2017 Hilmy Corp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var etName: UITextField!
+    @IBOutlet weak var etPass: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func btnLog(_ sender: Any) {
+        if (etName.text == "") && (etPass.text == "") {
+            let alertController = UIAlertController(title: "Warning",
+                                                    message: "The Column can't be Empty", preferredStyle : .alert)
+            alertController.addAction(UIAlertAction(title: "OK",
+                                                    style: .default,handler: nil))
+            present(alertController, animated: true, completion: nil)
+        } else if (etName.text == "rpl2idn") && (etPass.text == "kitabisa") {
+            func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+                //check what segue with name passData is available or not
+                if(segue.identifier == "checkData"){
+                    let sendData = segue.destination as! InputProfileViewController
+                }
+            }
+        }else{
+            let alertController = UIAlertController(title: "Warning",
+                                                    message: "Your Username and Profile is Wrong", preferredStyle : .alert)
+            alertController.addAction(UIAlertAction(title: "OK",
+                                                    style: .default,handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+        override func didReceiveMemoryWarning() {
+            super.didReceiveMemoryWarning()
+            // Dispose of any resources that can be recreated.
+}
+}
